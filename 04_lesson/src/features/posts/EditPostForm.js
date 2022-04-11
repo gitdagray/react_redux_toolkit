82d81f76a84faf1e7b1c6421536ae453f33c0d4d
gitdagray <DaveOnEleven@gmail.com>
2022-04-11 08:34:18 -0500

    Insured onAuthorChanged saves numeric value instead of a string value

--- 04_lesson/src/features/posts/EditPostForm.js
+++ 04_lesson/src/features/posts/EditPostForm.js
@@ -29,7 +29,7 @@ const EditPostForm = () => {
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
-    const onAuthorChanged = e => setUserId(e.target.value)
+    const onAuthorChanged = e => setUserId(Number(e.target.value))
 
     const canSave = [title, content, userId].every(Boolean) && requestStatus === 'idle';
 
