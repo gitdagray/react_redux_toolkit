ae191e64b42a9b738976a7affb9c25d0bafbc55a
gitdagray <DaveOnEleven@gmail.com>
2022-06-24 12:24:34 -0500

    Added Redux Devtools flag to store

--- 07_lesson/src/app/store.js
+++ 07_lesson/src/app/store.js
@@ -6,5 +6,6 @@ export const store = configureStore({
         [apiSlice.reducerPath]: apiSlice.reducer
     },
     middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware)
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
 })
