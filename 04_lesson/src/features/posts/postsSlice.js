ef530de8e3b89e6e0f58560ef8234be9fa7e0220
gitdagray <DaveOnEleven@gmail.com>
2022-08-12 11:43:35 -0500

    Added fix for jsonplaceholder API not returning accurate new post IDs after 101

--- 04_lesson/src/features/posts/postsSlice.js
+++ 04_lesson/src/features/posts/postsSlice.js
@@ -106,7 +106,18 @@ const postsSlice = createSlice({
                 state.error = action.error.message
             })
             .addCase(addNewPost.fulfilled, (state, action) => {
-                action.payload.id = state.posts[state.posts.length - 1].id + 1;
+                // Fix for API post IDs:
+                // Creating sortedPosts & assigning the id 
+                // would be not be needed if the fake API 
+                // returned accurate new post IDs
+                const sortedPosts = state.posts.sort((a, b) => {
+                    if (a.id > b.id) return 1
+                    if (a.id < b.id) return -1
+                    return 0
+                })
+                action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+                // End fix for fake API post IDs 
+
                 action.payload.userId = Number(action.payload.userId)
                 action.payload.date = new Date().toISOString();
                 action.payload.reactions = {
