798a28d018d5d90eae8e47d0917cafeac2a63782
asigithub <akulich@tut.by>
2023-02-19 11:24:57 +0300

    fix: Incorrect emotion reaction in new post

--- 04_lesson_starter/src/features/posts/postsSlice.js
+++ 04_lesson_starter/src/features/posts/postsSlice.js
@@ -100,10 +100,10 @@ const postsSlice = createSlice({
                 action.payload.date = new Date().toISOString();
                 action.payload.reactions = {
                     thumbsUp: 0,
-                    hooray: 0,
+                    wow: 0,
                     heart: 0,
                     rocket: 0,
-                    eyes: 0
+                    coffee: 0
                 }
                 console.log(action.payload)
                 state.posts.push(action.payload)
