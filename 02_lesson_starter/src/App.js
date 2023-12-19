84ee848a36a09a5af94c7a83015df1a555c6fa44
Alie Brubaker <findingalberta@gmail.com>
2023-12-19 14:26:42 -0800

    finished lesson 2

--- 02_lesson_starter/src/App.js
+++ 02_lesson_starter/src/App.js
@@ -1,8 +1,11 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
   return (
     <main className="App">
-
+      <AddPostForm />
+      <PostsList />
     </main>
   );
 }
---
+++ 02_lesson_starter/src/features/posts/TimeAgo.jsx
@@ -0,0 +1,20 @@
+import { parseISO, formatDistanceToNow } from "date-fns";
+
+import React from 'react'
+
+const TimeAgo = ({ timestamp }) => {
+    let timeAgo = ''
+    if (timestamp) {
+        const date = parseISO(timestamp)
+        const timePeriod = formatDistanceToNow(date)
+        timeAgo = `${timePeriod} ago`
+    }
+
+    return (
+        <span title={timestamp}>
+            &nbsp; <i>{timeAgo}</i>
+        </span>
+    )
+}
+
+export default TimeAgo
